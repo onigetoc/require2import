871da2e4ed9e72d7da18bf3f5eaccf76913e0d25
Gino <ginocote@gmail.com>
2024-05-09 21:04:13 -0400

    Delete src directory

--- src/sum.js
+++
@@ -1,9 +0,0 @@
-// https://www.npmjs.com/package/edit-package-json
-// https://stackoverflow.com/a/42407814/211324
-
-// var math = require('simple-test-package'); // ORIGINAL: Import type require
-var math = require('simple-test-package'); // Import type module
-
-const sum = math.sumNumbers(3, 5);
-
-console.log('result: '+sum); // 8 
