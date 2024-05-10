cf421717cc4760633615214c47e8befcbd463b3e
Gino <ginocote@gmail.com>
2024-05-09 21:08:30 -0400

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-// var math = require('simple-test-package');'; // ORIGINAL: Import type require
-import * as math from 'simple-test-package'; // Import type module
-
-const sum = math.sumNumbers(3, 5);
-
-console.log(sum); // 8 
