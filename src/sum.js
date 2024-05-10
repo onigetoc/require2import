28d5b5c3755a0a65a36bed82c51b5b629d2295be
Gino <ginocote@gmail.com>
2024-05-09 21:06:13 -0400

    Add files via upload

---
+++ src/sum.js
@@ -0,0 +1,6 @@
+// var math = require('simple-test-package'); // ORIGINAL: Import type commonjs
+
+// Install 'simple-test-package' to test real npm module
+const  math = require('simple-test-package'); 
+
+var sum = math.sumNumbers(3, 5);
